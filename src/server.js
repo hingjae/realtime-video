11a5f30b0b169adcc91ebb0504ca9c57755595c7
leejae honey <ljh23270@naver.com>
2022-07-29 17:12:02 +0900

    set camera

---
+++ src/server.js
@@ -0,0 +1,24 @@
+import http from "http";
+import express from "express";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
+
+const app = express();
+const PORT = 3000;
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => {
+  res.render("home");
+});
+app.get("/*", (_, res) => {
+  res.redirect("/");
+});
+
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
+
+httpServer.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
