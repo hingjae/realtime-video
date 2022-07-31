e733d9ccaa31abf89b46b4171d139d3cdd9f37a0
leejae honey <ljh23270@naver.com>
2022-07-31 12:47:40 +0900

    video setup

--- src/server.js
+++ src/server.js
@@ -19,6 +19,22 @@ app.get("/*", (_, res) => {
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
+wsServer.on("connection", (socket) => {
+  socket.on("join_room", (roomName) => {
+    socket.join(roomName);
+    socket.to(roomName).emit("welcome");
+  });
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
+  });
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
+  });
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
+  });
+});
+
 httpServer.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
 });
